2d0022c66ca1a751365570e2e53cb1c3fdb812c7
MichaelHu <michael.hu@withinspace.com>
2020-08-24 15:18:41 +0800

    chnage service provider

--- src/Services/LandlordService/src/Providers/LandlordServiceProvider.php
+++ src/Services/LandlordService/src/Providers/LandlordServiceProvider.php
@@ -5,6 +5,7 @@ namespace ArtisanCloud\SaaSFramework\Services\LandlordService\src\Providers;
 use Illuminate\Support\ServiceProvider;
 use ArtisanCloud\SaaSFramework\Services\LandlordService\src\Contracts\LandlordServiceContract;
 use ArtisanCloud\SaaSFramework\Services\LandlordService\src\LandlordService;
+use Laravel\Passport\Passport;
 
 /**
  * Class LandlordServiceProvider
@@ -33,6 +34,9 @@ class LandlordServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // make sure passport is installed
+        $this->configPassport();
+
         if ($this->app->runningInConsole()) {
               // publish config file
 //              $this->publishes([
@@ -47,6 +51,18 @@ class LandlordServiceProvider extends ServiceProvider
                 ], ['SaaSFramework', 'Landlord-Migration']);
               }
             }
+
+    }
+
+    protected function configPassport()
+    {
+        // make sure passport is installed
+        Passport::routes(function ($router) {
+            // RouteRegistrar->forAccessTokens()
+            $router->forAccessTokens();
+        }, ['middleware' => 'checkHeader']);
+        Passport::tokensExpireIn(now()->addDays(90));
+        Passport::refreshTokensExpireIn(now()->addDays(90));
 
     }
 }
