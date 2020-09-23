9f9c4de08c5235e276e5a7c08d9a1406029ae4c4
michaelhu <hbj148@gmail.com>
2020-09-23 23:03:19 +0800

    add alias for middlewares

---
+++ src/Http/Middleware/CheckClientHavingUser.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Controllers\API\APIResponse;
+use App\Services\UserService;
+use Closure;
+
+class CheckClientHavingUser
+{
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        $user = \Auth::guard('api')->user();
+        UserService::setAuthUser($user);
+
+        return $next($request);
+    }
+}
