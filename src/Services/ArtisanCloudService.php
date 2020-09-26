dcc14444b45cd857c95f1ff31f53fa2606cff640
michaelhu <hbj148@gmail.com>
2020-09-26 15:30:31 +0800

    add hierachable trait

--- src/Services/ArtisanCloudService.php
+++ src/Services/ArtisanCloudService.php
@@ -251,47 +251,7 @@ class ArtisanCloudService
     }
 
 
-    /**
-     * Get children tree list with item id and specified the level count
-     *
-     * @param mixed $parent
-     * @param int $status
-     * @param array $attributes
-     * @param int $level
-     *
-     * @return mixed
-     */
-    function getTreeList($parent = NULL, int $status = NULL, array $attributes, int $level = 5): ?array
-    {
-        // level counter --
-        if ($level < 0) {
-            return NULL;
-        } else {
-            $level = $level - 1;
-        }
 
-
-        // get parent node's children nodes
-        $arrayModel = array();
-        $qb = self::GetItemsBy(
-            get_class($parent),
-            ['parent_uuid' => ($parent ? $parent->uuid : null)]
-        );
-        if (is_null($status)) $qb->whereStatus($status);
-        $collectionModel = $qb->get();
-//    	dump($collectionChildren);
-
-
-        // iterate children node with sub-children nodes
-        foreach ($collectionModel as $key => $model) {
-//      		dump($model);
-            $model->load($attributes);
-            $model->children = $this->getTreeList($model, $status, $attributes, $level);
-
-            array_push($arrayModel, $model);
-        }
-        return $arrayModel;
-    }
 
 
     /**--------------------------------------------------------------- Cache Query  -------------------------------------------------------------*/
@@ -368,30 +328,6 @@ class ArtisanCloudService
 
     }
 
-    /**
-     * Get cached children tree list with item id and specified the level count
-     *
-     * @param mixed $parent
-     * @param int $status
-     * @param array $attributes
-     * @param int $level
-     *
-     * @return mixed
-     */
-    function getCachedTreeList($parent = NULL, int $status = NULL, array $attributes, int $level = 5): ?array
-    {
-        $cacheTag = $this->m_model->getCacheTag();
-        $cacheKey = $this->m_model->getItemCacheKey($parent->uuid . '.children');
-//        dd($cacheTag, $cacheKey, CacheTimeout::CACHE_TIMEOUT_MONTH);
-        $cachedList = Cache::tags($cacheTag)->remember($cacheKey, CacheTimeout::CACHE_TIMEOUT_MONTH, function () use ($parent, $status, $attributes, $level) {
-
-            \Log::info("request " . class_basename($parent) . ":{$parent->uuid} tree list here status:{$status}, level:{$level}");
-            $list = $this->getTreeList($parent, $status, $attributes, $level);
-
-            return $list;
-        });
-
-        return $cachedList;
-    }
+    
 
 }
